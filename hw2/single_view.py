38b1b063499600ee24f62b0ca835ef13f017e001
kyu <lmk3224@kaist.ac.kr>
2025-04-04 18:33:48 +0900

    sampling iteration modified

--- hw2/single_view.py
+++ hw2/single_view.py
@@ -11,10 +11,10 @@ def normalize_homogeneous(p):
     return p / p[2] if p[2] != 0 else p
 
 
-def compute_vanishing_points(p1, p2, p3, p4):
-    v1 = normalize_homogeneous(cross_product(cross_product(p1, p2), cross_product(p3, p4)))
-    v2 = normalize_homogeneous(cross_product(cross_product(p1, p3), cross_product(p2, p4)))
-    return v1, v2
+def compute_vanishing_points(point):
+    _v1 = normalize_homogeneous(cross_product(cross_product(point[0], point[1]), cross_product(point[2], point[3])))
+    _v2 = normalize_homogeneous(cross_product(cross_product(point[0], point[2]), cross_product(point[1], point[3])))
+    return _v1, _v2
 
 
 def compute_height(ref_top, ref_bottom, human_bottom, human_top, R, v1, v2):
@@ -61,25 +61,29 @@ if __name__ == "__main__":
     os.makedirs(results_dir, exist_ok=True)
 
     print("Select 4 points for vanishing point computation (p1 to p4)")
-    vanishing_points = get_points_from_image(image, 4, "Select Vanishing Points")
-    p1, p2, p3, p4 = vanishing_points
+
+    num_sampling = 5
+    points = []
+    vanishing_points = list(get_points_from_image(image, 4 * num_sampling, "Select Vanishing Points"))
+    for i in range(4):
+        points.append(np.mean(vanishing_points[num_sampling*i:num_sampling*(i+1)], axis=0))
 
     # Compute vanishing points
-    v1, v2 = compute_vanishing_points(p1, p2, p3, p4)
+    v1, v2 = compute_vanishing_points(points)
 
     # Show selected points and vanishing points
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    plt.scatter([p1[0], p2[0], p3[0], p4[0], v1[0], v2[0]],
-                [p1[1], p2[1], p3[1], p4[1], v1[1], v2[1]],
+    plt.scatter([points[0][0], points[1][0], points[2][0], points[3][0], v1[0], v2[0]],
+                [points[0][1], points[1][1], points[2][1], points[3][1], v1[1], v2[1]],
                 color=['red', 'red', 'red', 'red', 'blue', 'blue'],
                 s=[50, 50, 50, 50, 50, 50])
     # red lines
-    red_lines = [(p1, p2), (p3, p4)]
+    red_lines = [(points[0], points[1]), (points[2], points[3])]
     for start, end in red_lines:
         plt.plot([start[0], end[0]], [start[1], end[1]], 'r-', linewidth=2)
 
     # blue lines
-    blue_lines = [(p2, v1), (p4, v1), (p1, v2), (p2, v2)]
+    blue_lines = [(points[1], v1), (points[3], v1), (points[0], v2), (points[1], v2)]
     for start, end in blue_lines:
         plt.plot([start[0], end[0]], [start[1], end[1]], 'b-', linewidth=1)
 
@@ -100,8 +104,8 @@ if __name__ == "__main__":
 
     # Display result
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    plt.scatter([p1[0], p2[0], p3[0], p4[0], ref_top[0], ref_bottom[0], human_bottom[0], human_top[0]],
-                [p1[1], p2[1], p3[1], p4[1], ref_top[1], ref_bottom[1], human_bottom[1], human_top[1]], color='red')
+    plt.scatter([points[0][0], points[1][0], points[2][0], points[3][0], ref_top[0], ref_bottom[0], human_bottom[0], human_top[0]],
+                [points[0][1], points[1][1], points[2][1], points[3][1], ref_top[1], ref_bottom[1], human_bottom[1], human_top[1]], color='red')
     plt.title(f"Estimated Height: {H:.2f} cm")
     plt.savefig(fig_name_2, dpi=300, bbox_inches='tight')
     plt.show()
